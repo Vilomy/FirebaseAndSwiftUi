b28b15be1077abce0c67f77eef4835924f27d7a4
Gleb Zadonskiy <vilomy2@gmail.com>
2020-08-16 01:40:26 +0300

    Initial Commit

---
+++ FirebaseSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseSwiftUI
+//
+//  Created by Gleb Zadonskiy on 16.08.2020.
+//  Copyright © 2020 Gleb Zadonskiy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
